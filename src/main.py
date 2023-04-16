6e9211220cdd9c395a85ee0f4a535a771371f1e0
suynep <suy.nepal@gmail.com>
2023-04-16 19:50:25 +0545

    default

--- src/main.py
+++ src/main.py
@@ -5,18 +5,28 @@ import colors
 curr = os.listdir('.')
 prev = os.listdir('/')
 
+dir = []
+dir.append(curr)
+dir.append(prev)
 but = []
 
 palette = colors.colors
 
-for c in prev:
-    button = urwid.Text(('bright_cyan', c), align='center')
+def show_dir(dir_path):
+    for c in dir_path:
+        txt = urwid.Text(('bright_cyan', c), align='center')
+        but.append(urwid.AttrMap(txt, 'bg'))
 
-    but.append(urwid.AttrMap(button, 'bg'))
+show_dir(prev)
+def key_press(key):
+    x = 0
+    if key in ('j'):
+        show_dir(dir[x%2])
+        x += 1
 
 print(palette)
 
 main = urwid.ListBox(urwid.SimpleFocusListWalker(but))
-loop = urwid.MainLoop(main, palette)
+loop = urwid.MainLoop(main, palette, unhandled_input=key_press)
 loop.screen.set_terminal_properties(colors=256)
 loop.run()
