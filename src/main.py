c3eeacd5624eb157821d90f0abaa5b2ad7e8f015
suynep <suy.nepal@gmail.com>
2023-04-18 16:39:34 +0545

    added functionality/oop addition

--- src/main.py
+++ src/main.py
@@ -1,32 +1,68 @@
-import urwid 
+#!/usr/bin/env python
 import os
-import colors
+import urwid
 
-curr = os.listdir('.')
-prev = os.listdir('/')
+palette = [
+    (None,  'light gray', 'black'),
+    ('reversed', 'standout', ''),
+    ('body', 'yellow', 'light blue'),
+    ('bodyrev', 'yellow', 'dark red'),
+    ('dirs', 'black', 'light blue'),
+    ('dirsrev', 'white', 'dark red'),
+    ('bg', 'black', 'dark blue'),]
 
-dir = []
-dir.append(curr)
-dir.append(prev)
-but = []
+class BrowserPanel(urwid.WidgetPlaceholder):
+    def __init__(self, path):
+        self.rootPath = '/'
+        self.previousPath = self.rootPath
+        self.currentPath = path
+        self.fileList = sorted(os.listdir(self.currentPath))
+        body = self.create_buttons()
+        super(BrowserPanel, self).__init__(body)
 
-palette = colors.colors
+    def update_file_list(self, choice):
+        if choice == '..':
+            newPath = os.path.dirname(self.currentPath)
+        else:
+            newPath = os.path.join(os.path.join(self.currentPath, choice))
+        if os.path.isdir(newPath):
+            self.previousPath = self.currentPath
+            self.currentPath = newPath
+        try:
+            self.fileList = os.listdir(self.currentPath)
+        except:
+            self.currentPath = self.previousPath
+            self.fileList = os.listdir(self.currentPath)
+        if self.currentPath != '/':
+            self.fileList.insert(0, '..')
 
-def show_dir(dir_path):
-    for c in dir_path:
-        txt = urwid.Text(('bright_cyan', c), align='center')
-        but.append(urwid.AttrMap(txt, 'bg'))
+    def create_buttons(self):
+        body = []
+        for oneFile in sorted(self.fileList):
+            button = urwid.Button(oneFile)
+            if os.path.isdir(os.path.join(self.currentPath, oneFile)):
+                urwid.connect_signal(button, 'click', self.update_body, oneFile)
+                body.append(urwid.AttrMap(button, 'dirs', focus_map='dirsrev'))
+            else:
+                body.append(urwid.AttrMap(button, None, focus_map='bodyrev'))
+        return urwid.AttrMap(urwid.LineBox(urwid.ListBox(
+            urwid.SimpleFocusListWalker(body)), self.currentPath), 'body')
+    
+    def update_body(self, button, choice):
+        self.update_file_list(choice)
+        self.original_widget = self.create_buttons()
 
-show_dir(prev)
-def key_press(key):
-    x = 0
-    if key in ('j'):
-        show_dir(dir[x%2])
-        x += 1
+def exit_program(key):
+    if key == 'q':
+        raise urwid.ExitMainLoop()
 
-print(palette)
+def main():
+    panel1 = BrowserPanel('/')
+    panel2 = BrowserPanel('/')
+    main = urwid.Columns([panel1, panel2])
+    loop = urwid.MainLoop(main, palette, unhandled_input=exit_program)
+    loop.run()
 
-main = urwid.ListBox(urwid.SimpleFocusListWalker(but))
-loop = urwid.MainLoop(main, palette, unhandled_input=key_press)
-loop.screen.set_terminal_properties(colors=256)
-loop.run()
+if __name__ == '__main__':
+    main()
+
